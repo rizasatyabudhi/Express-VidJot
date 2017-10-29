9624f52e068655d4ce8459a607e235b3afc324fb
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-10-29 12:49:06 +0700

    handlebars & forms

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -7,7 +8,7 @@ const app = express();
 // Map global promise - get rid of depricated warning
 mongoose.Promise = global.Promise;
 
-//// Connect to Mongoose ////
+// Connect to Mongoose
 mongoose
   .connect("mongodb://localhost/vidjot-dev", {
     useMongoClient: true
@@ -19,17 +20,51 @@ mongoose
     console.log(error);
   });
 
-//// Handlebars Middleware ////
+// Load Idea model
+require("./models/Idea");
+const Idea = mongoose.model("ideas");
+
+// Handlebars Middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//// Routes ////
+// bodyParser Middleware
+// bodyParser is required to access submitted data from client
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
+});
+
+app.get("/ideas/add", (req, res) => {
+  res.render("ideas/add");
+});
+
+// Process Form
+app.post("/ideas", (req, res) => {
+  const errors = [];
+  if (!req.body.title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!req.body.details) {
+    errors.push({ text: "Please add some details" });
+  }
+
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send("Passed");
+  }
 });
 
 const port = 5000;
