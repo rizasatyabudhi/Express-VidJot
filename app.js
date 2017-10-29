56912664d3db0f57516ba7cf5532b0c8955ed983
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-10-29 12:02:11 +0700

    connect mongodb

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 const express = require("express");
-const app = express();
-const port = 5000;
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 
+const app = express();
+
+// Map global promise - get rid of depricated warning
+mongoose.Promise = global.Promise;
+
+//// Connect to Mongoose ////
+mongoose
+  .connect("mongodb://localhost/vidjot-dev", {
+    useMongoClient: true
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+//// Handlebars Middleware ////
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//// Routes ////
 app.get("/", (req, res) => {
-  res.send("INDEX");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.send("ABOUT");
+  res.render("about");
 });
 
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
